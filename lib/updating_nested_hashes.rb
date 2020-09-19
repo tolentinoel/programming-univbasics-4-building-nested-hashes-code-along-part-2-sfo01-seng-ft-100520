c02e681aef216e76a6cd7f0da7aec733f626a2dd
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-19 00:02:02 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -62,7 +62,7 @@ def add_characters
 
 epic_tragedy[:additional_characters] = Array.new
 
-epic_tragedy[:additional_characters].push({name: "Prince Escalus")
+epic_tragedy[:additional_characters].push({name: "Prince Escalus"})
 
 
 
